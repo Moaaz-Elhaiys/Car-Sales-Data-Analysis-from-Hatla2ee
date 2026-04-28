0d9112de221d1a704875d55daaa7a4204e523a72
Moaaz Elhaiys <moaaz0744@gmail.com>
2026-04-28 22:00:46 +0300

    spider edited

--- cars/spiders/cars_spider.py
+++ cars/spiders/cars_spider.py
@@ -1,37 +1,32 @@
 import scrapy
 from cars.items import CarItem
 from urllib.parse import urlencode
-import random
 
-user_agent_list = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
-    'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363',
-]
-
-headers = {"User-Agent": random.choice(user_agent_list)}
 
 class CarsSpider(scrapy.Spider):
     name = "cars"
     allowed_domains = ["eg.hatla2ee.com"]
-    start_urls = ["https://eg.hatla2ee.com/en/car"]
     seen_urls = set()
+
+    # DELETE the start_urls line and add this method instead:
+    def start_requests(self):
+        url = "https://eg.hatla2ee.com/en/car"
+        # This meta tag is what actually turns on the anti-bot bypass!
+        yield scrapy.Request(url, callback=self.parse, meta={"impersonate": "chrome110"})
 
     def parse(self, response):
         base_url = "https://eg.hatla2ee.com"
 
-        car_links = response.xpath("//div[@class='newCarListUnit_header']/span/a/@href").getall()
+        car_links = response.xpath("//div[@data-slot='card-content']//a/@href").getall()
 
         for car_link in car_links:
             car_url = base_url + car_link
-            yield response.follow(url=car_url, callback=self.parse_car_page, headers=headers)
+            yield response.follow(url=car_url, callback=self.parse_car_page, meta={"impersonate": "chrome110"})
 
         next_page = response.xpath("//div[@class='pagination pagination-right']//li[@class='active']/following-sibling::li[1]/a/@href").get()
         if next_page and "page" in next_page:
             next_page_url = base_url + next_page
-            yield response.follow(url=next_page_url, callback=self.parse, headers=headers)
+            yield response.follow(url=next_page_url, callback=self.parse, meta={"impersonate": "chrome110"})
             
     def parse_car_page(self, response):
         car_item = CarItem()
